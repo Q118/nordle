63c8ccec29d9e21d5c3a820b3de7f19715cd240f
Shelby Rothman <shelby.rothman@aderant.com>
2022-03-23 16:25:58 -0700

    removed unneeded vars

--- app.js
+++ app.js
@@ -14,26 +14,32 @@ const wordlePrompt = {
     validate: value => value.length != 5 ? 'Word must be 5 letters' : true
 };
 
-const check = (guess) => {
+const check = async (guess) => {
     let results = [];
     // loop over each letter in the word
     for (let i in guess) {
-        let attempt = {
-            letter: guess[i],
-            color: "bgGrey"
-        };
-        // check if the letter at the specified index in the guess word exactly matches the letter at the specified index in the puzzle
-        if (attempt.letter.toUpperCase() === puzzle[i].toUpperCase()) {
-            process.stdout.write(chalk.white.bgGreen.bold(` ${guess[i]} \t`));
-            continue;
+        try {
+            let attempt = {
+                letter: guess[i],
+                color: "bgGrey"
+            };
+            // check if the letter at the specified index in the guess word exactly matches the letter at the specified index in the puzzle
+            if (attempt.letter.toUpperCase() === puzzle[i].toUpperCase()) {
+                process.stdout.write(chalk.white.bgGreen.bold(` ${guess[i]} \t`));
+                // ensure that above gets run even when the win
+
+                continue;
+            }
+            // check if the letter at the specified index in the guess word is at least contained in the puzzle at some other position
+            if (puzzle.toUpperCase().includes(attempt.letter.toUpperCase())) {
+                process.stdout.write(chalk.white.bgYellow.bold(` ${guess[i]} \t`));
+                continue;
+            }
+            // otherwise the letter doesn't exist at all in the puzzle
+            process.stdout.write(chalk.white.bgGrey.bold(` ${guess[i]} \t`));
+        } catch (error) {
+            console.error(error);
         }
-        // check if the letter at the specified index in the guess word is at least contained in the puzzle at some other position
-        if (puzzle.toUpperCase().includes(attempt.letter.toUpperCase())) {
-            process.stdout.write(chalk.white.bgYellow.bold(` ${guess[i]} \t`));
-            continue;
-        }
-        // otherwise the letter doesn't exist at all in the puzzle
-        process.stdout.write(chalk.white.bgGrey.bold(` ${guess[i]} \t`));
     }
     return results;
 }
@@ -48,7 +54,12 @@ const play = async (tries) => {
         // cl(`${puzzle} is puzzle inside of play`)
         // cl(`${guess} is guess inside of play`)
         if (guess == puzzle.toUpperCase()) {// if the word matches, they win!
-            cl("WINNER!");
+            // we want this back in the first conditional in check()
+            for (let i in guess) {
+                process.stdout.write(chalk.white.bgGreen.bold(` ${guess[i]} \t`));
+            }
+            process.stdout.write("\n");
+            cl(chalk.white.bgBlueBright.bold("You win!\n"));
         } else {
             check(guess);
             // this forces std out to print out the results for the last guess
@@ -71,7 +82,7 @@ async function main() {
     // then get the random word with letter
     sendCall(letter).then((word) => {
         puzzle = word;
-        // cl(puzzle, "in then");
+        cl(` ${puzzle} in callback  `);
         //! uncomment above to see the answer 
         afterMain(); // start the game after the puzzle is set
     }).catch((err) => {
--- helpers/prompts.js
+++
@@ -1,8 +0,0 @@
-const wordlePrompt = {
-    type: 'text',
-    name: 'word',
-    message: 'Enter a 5 letter word:',
-    validate: value => value.length === 5 ? true : 'Please enter a 5 letter word.'
-};
-
-module.exports = wordlePrompt;
